dbb1e3cc49e46c7203c7d91df68cfec1b8b067c8
irina-kavun <kavunirina@gmail.com>
2020-03-02 22:44:09 +0200

    Updates

---
+++ precache-manifest.23264797d10f9f695cd82048e603b5cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41344d974afe6b50e5231b86a936f45",
+    "url": "/react-calendar/index.html"
+  },
+  {
+    "revision": "773df6168ce7b268722d",
+    "url": "/react-calendar/static/css/2.6fea60a8.chunk.css"
+  },
+  {
+    "revision": "9664cc334b189f43600c",
+    "url": "/react-calendar/static/css/main.e8aee22d.chunk.css"
+  },
+  {
+    "revision": "773df6168ce7b268722d",
+    "url": "/react-calendar/static/js/2.0cba2285.chunk.js"
+  },
+  {
+    "revision": "f89c9b0960761b2023f3041c41c07842",
+    "url": "/react-calendar/static/js/2.0cba2285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9664cc334b189f43600c",
+    "url": "/react-calendar/static/js/main.7de69d75.chunk.js"
+  },
+  {
+    "revision": "a40e468125106b795c3c",
+    "url": "/react-calendar/static/js/runtime-main.350b7332.js"
+  }
+]);
